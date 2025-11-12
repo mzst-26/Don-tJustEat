345e4e0fb7ef4e7787ea2848617161873ee3541b
Mobin Z <115487390+mzst-26@users.noreply.github.com>
2025-11-12 15:48:31 +0000

    Back button added to the sign in pages.

--- app/src/main/java/com/example/dontjusteat/customer_login.java
+++ app/src/main/java/com/example/dontjusteat/customer_login.java
@@ -1,6 +1,9 @@
 package com.example.dontjusteat;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class customer_login extends AppCompatActivity {
@@ -10,5 +13,9 @@ public class customer_login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // This line loads your XML layout and displays it on the screen
         setContentView(R.layout.customer_login);
+
+        //Back button
+        ImageView backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> finish());
     }
 }
