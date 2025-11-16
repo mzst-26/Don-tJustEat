90deba4e2d51df09bfdaa7eb506a3fc2388cc1f9
Mobin Z <115487390+mzst-26@users.noreply.github.com>
2025-11-16 12:42:38 +0000

    Customer_Booking_Search_Layout
    Search header layout added.
    Updated customer_login to navigate to customer_booking on email sign-in. Added new styles for booking UI and registered the activity in AndroidManifest.xml.

---
+++ app/src/main/java/com/example/dontjusteat/customer_booking.java
@@ -0,0 +1,17 @@
+package com.example.dontjusteat;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class customer_booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // This line loads your XML layout and displays it on the screen
+        setContentView(R.layout.customer_booking);
+    }
+}
