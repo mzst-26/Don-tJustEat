0ae8636d45b25e8191c320c8a56409e933311e7d
Mobin Z <115487390+mzst-26@users.noreply.github.com>
2025-12-03 22:37:24 +0000

    Add booking summary screen and update UI styles
    Introduced booking_summary activity and layout, enabling navigation from customer_location_detail to booking_summary. Updated UI elements in customer_booking and customer_location_detail layouts for improved styling and color consistency. Added a new small primary button style in themes.xml and adjusted location_icon tint to use theme color.

---
+++ app/src/main/java/com/example/dontjusteat/booking_summary.java
@@ -0,0 +1,15 @@
+package com.example.dontjusteat;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class booking_summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.booking_summary);
+
+    }
+}
